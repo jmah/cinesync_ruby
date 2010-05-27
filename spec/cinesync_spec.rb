b49df49e97f28a56fb964106ae0670e24a28b584
Jonathon Mah <jmah@cinesync.com>
2010-05-27 14:01:40 +0930

    Added spec for short_hash utility method

--- spec/cinesync_spec.rb
+++ spec/cinesync_spec.rb
@@ -1,7 +1,12 @@
+$: << File.expand_path(File.dirname(__FILE__))
 require 'spec_helper'
 
-describe "cineSync" do
-  it "fails" do
-    should.flunk "hey buddy, you should probably rename this file and start specing for real"
+describe "cineSync utilities" do
+  it "correctly hashes big file" do
+    CineSync.short_hash("/Volumes/Oyama/Streams/cineSync Test Files/movies/nasa_shuttle_m420p.mov").should == "08e5628d51b14278f73f36afebf0506afc2bfcf8"
+  end
+
+  it "correctly hashes small file" do
+    CineSync.short_hash("spec/small.txt").should == '1e40efb84050f92618e2c430742f44254771cf6b'
   end
 end
